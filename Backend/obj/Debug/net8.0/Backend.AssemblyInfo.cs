c40be4d818dd3b97cad04dc9ea21fba10434fdf9
beadidd <57589034+FlameProfi@users.noreply.github.com>
2026-01-25 02:33:13 +0300

    small update commit

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620201515e9d8fa97aef860e7c5775e8806dc717")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+846a0d9f0a4db9c94aafc7f75674fb2f94d39590")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
